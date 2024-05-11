ddd005ed47dd418e3e1db0e6dbf61eb8c47caa4a
isaac <isaackoz130@gmail.com>
2024-05-10 18:24:01 -0700

    add names

--- web/index.php
+++ web/index.php
@@ -8,7 +8,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1 class='text-5xl font-bold '>CPSC 332<br/> Term Project</h1>
+    <h1 class='text-5xl font-bold  p-12'>CPSC 332<br/> Term Project</h1>
+    <p>By Isaac Koczwara and Anthony Lapan</p>
     <ul>
         <li><a href="professor.php" class="button-9">Professor's Portal</a></li>
         <li><a href="student.php" class="button-9">Student's Portal</a></li>
